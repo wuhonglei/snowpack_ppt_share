9f7e9d92fd98c48c1d9b8325d34e03a33ee79f63
wuhonglei <1017368065@qq.com>
2020-08-27 08:24:01 +0800

    提交测试代码

--- click-away/src/App.vue
+++ click-away/src/App.vue
@@ -1,50 +1,48 @@
 <template>
-	<div id="wrapper">
-		<button @click="showMenu=!showMenu" v-on-clickaway="away">展开菜单</button>
-		<ul v-if="showMenu">
-			<li>item1</li>
-			<li>item2</li>
-			<li>item3</li>
-		</ul>
+	<div id="app">
+		<div v-on-clickaway="away" class="menu">
+			<button @click="showMenu=!showMenu">展开菜单</button>
+			<ul v-if="showMenu">
+				<li>item1</li>
+				<li>item2</li>
+				<li>item3</li>
+			</ul>
+		</div>
 	</div>
 </template>
 
 <script>
 import { mixin as clickaway } from "vue-clickaway";
-import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
 	name: "App",
-	components: {
-		HelloWorld,
-	},
 	mixins: [clickaway],
 	data() {
 		return {
-			name: "wuhonglei",
 			showMenu: false,
 		};
 	},
-	created() {
-		console.info("created");
-	},
 	methods: {
-		getName() {
-			return this.name + "1";
-		},
 		away() {
-			console.info("click aray");
+			console.info("click away");
+			this.showMenu = false;
 		},
 	},
 };
 </script>
 
 <style>
-#wrapper {
+#app {
 	font-family: Avenir, Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
-	margin-top: 61px;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+
+.menu {
+	width: 250px;
+	border: 1px solid #ddd;
 }
 
 ul {
